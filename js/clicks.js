f19c85e6a45681db8b63b9a0cc9a8124d2377bf3
Dave Syiemlieh <recd2dmax@gmail.com>
2019-01-17 18:10:13 +0530

    js updated

--- js/clicks.js
+++ js/clicks.js
@@ -1,9 +1,12 @@
 var modal = document.getElementsByClassName("modal");
+<<<<<<< HEAD
 
 function openmod(i) {
   modal[i].style.display = "block";
 }
+=======
 function openmod(i) { modal[i].style.display = "block"; }
 
 var mmodal = document.getElementsByClassName("mmodal");
 function openmod2(i) { mmodal[i].style.display = "block"; }
+>>>>>>> 8551843030d011b8a33f22c1b92debd674742e26
