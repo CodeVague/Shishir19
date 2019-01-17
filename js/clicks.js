8551843030d011b8a33f22c1b92debd674742e26
unknown <recd2dmax@gmail.com>
2019-01-17 16:09:22 +0530

    Day 2 Events added

--- js/clicks.js
+++ js/clicks.js
@@ -1,2 +1,5 @@
 var modal = document.getElementsByClassName("modal");
-function openmod(i) {modal[i].style.display="block";}
+function openmod(i) { modal[i].style.display = "block"; }
+
+var mmodal = document.getElementsByClassName("mmodal");
+function openmod2(i) { mmodal[i].style.display = "block"; }
