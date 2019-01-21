5af67b0d2f42dd7c7702c18f9e93d1a3b2475089
Davien <recd2dmax@gmail.com>
2019-01-21 12:54:03 +0530

    added menu for smaller screens

--- js/clicks.js
+++ js/clicks.js
@@ -3,3 +3,6 @@ function openmod(i) { modal[i].style.display = "block"; }
 
 var mmodal = document.getElementsByClassName("mmodal");
 function openmod2(i) { mmodal[i].style.display = "block"; }
+
+function drop(){document.getElementById("drop-menu").style.display = "block";}
+function undrop(){document.getElementById("drop-menu").style.display = "none";}
