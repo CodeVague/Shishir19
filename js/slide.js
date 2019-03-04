da80e524112de34225f254ce5764d91baf1c68fc
Dave <recd2dmax@gmail.com>
2019-03-04 23:19:19 +0530

    modified slide show template

--- js/slide.js
+++ js/slide.js
@@ -1,23 +1,13 @@
 var slides = document.getElementsByClassName("slide-item");
-var images = ["img/logo.jpg", "img/arrow.png", "img/nitm-logo.jpg"];
+var images1 = ["img/logo.jpg", "img/arrow.png", "img/nitm-logo.jpg"];
 var count = 0;
-var len = images.length;
+var len1 = images1.length;
 
 function slide() {
-    slides[0].src = images[(count + 1) % len];
-    slides[1].src = images[(count + 2) % len];
-    slides[2].src = images[(count + 3) % len];
-    count = (count + 1) % len;
-}
-
-function slideup(){
-    slides[0].src = images[(count + 2) % len];
-    slides[1].src = images[(count) % len];
-    slides[2].src = images[(count + 1) % len];
-    if(count-1<0)
-        count = 2;
-    else
-        count = (count-1)%len;
+    slides[0].src = images1[(count + 1) % len1];
+    slides[1].src = images1[(count + 2) % len1];
+    slides[2].src = images1[(count + 3) % len1];
+    count = (count + 1) % len1;
 }
 
 setInterval(slide, 5000);
