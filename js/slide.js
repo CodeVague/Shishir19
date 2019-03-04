4126db9f98f0518145605c73f4c0132e5aa64e25
Dave <recd2dmax@gmail.com>
2019-03-04 23:37:16 +0530

    added pictures for slideshow

--- js/slide.js
+++ js/slide.js
@@ -1,13 +1,17 @@
 var slides = document.getElementsByClassName("slide-item");
-var images1 = ["img/logo.jpg", "img/arrow.png", "img/nitm-logo.jpg"];
+var images1 = ["img/Drama1.jpg", "img/EBSB2.jpg", "img/EBSB3.jpg","img/EBSB4.jpg"];
+var images2 = ["img/Harmony2.jpg","img/Harmony5.jpg","img/Pronite1.jpg","img/Pronite2.jpg"];
+var images3 = ["img/Panache5.jpg","img/Panache10.jpg","img/Panache7.jpg","img/Panache22.jpg"];
+var images4 = ["img/su2.jpg","img/su7.jpg","img/su10.jpg","img/su11.jpg"];
+
 var count = 0;
-var len1 = images1.length;
 
 function slide() {
-    slides[0].src = images1[(count + 1) % len1];
-    slides[1].src = images1[(count + 2) % len1];
-    slides[2].src = images1[(count + 3) % len1];
-    count = (count + 1) % len1;
+    ++count;
+    slides[0].src = images1[(count) % 4];
+    slides[1].src = images2[(count) % 4];
+    slides[2].src = images3[(count) % 4];
+    slides[3].src = images4[(count) % 4];
 }
 
-setInterval(slide, 5000);
+setInterval(slide, 4000);
